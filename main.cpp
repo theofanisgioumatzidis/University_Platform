8ee0502866f31b5bf4128ac03f96c7c4ee25a346
theofanisgioumatzidis <108617392+theofanisgioumatzidis@users.noreply.github.com>
2024-06-09 04:48:15 +0300

    Login,printing tables

--- main.cpp
+++ main.cpp
@@ -1,18 +1,122 @@
 #include <iostream>
 #include <sqlite3.h>
 #include <filesystem>
+#include <string>
+#include <vector>
+#include <string>
+#include <iomanip>
 
 using namespace std;
+
+// Structure to hold the table data
+struct TableData {
+    vector<string> columnNames;
+    vector<vector<string>> rows;
+};
+
 void init_db();
 void bootstrap_db();
-int main(){
+static int callback(void* data, int argc, char** argv, char** azColName);
+void login(int &User, string &User_Capacity, int &error_login);
+void Print_Query(int User_id,string sql, int error);
 
+
+
+int main(){
+    // Creating the sql database if it doesnt exist and filling with data
+    if(!filesystem::exists("University_Platform.db")){
+            init_db();
+    bootstrap_db();
+    }
+    int User_id {-1};
+    string User_Capacity;
 
     cout << "Wellcome to the University Platform" << endl;
-    if(!filesystem::exists("University_Platform.db")){
-        // Creating an sql database if it doesnt exist
-        init_db();
-        bootstrap_db();
+
+
+    while(true){
+            int error_login {};
+            login(User_id, User_Capacity, error_login);
+            if(!error_login)
+                break;
+            cout << "Invalid username or password." << endl;
+    }
+    cout << "Login successful!" << endl;
+
+
+
+    while(User_Capacity == "admin"){
+                string action;
+                cout << "What you want to do? [See users -> SEE ,Edit entity -> EDIT, Delete entity -> DELETE, Stop -> STOP]" << endl;
+                cin >> action;
+
+                if (action == "Stop"){
+                    break;
+                }else if( action == "SEE"){
+                        int error {};
+                        string sql;
+                        sql =              string("SELECT FirstName, LastName, Age, Wage, StartDate, NULL AS StudyYear "
+                                           "FROM Employees "
+                                           "UNION ALL "
+                                           "SELECT FirstName, LastName, Age, NULL AS Wage, StartDate, StudyYear "
+                                           "FROM Students "
+                                           ";");
+                        Print_Query(User_id,sql,error);
+                }
+                break;
+    }
+    while(User_Capacity == "Boarding"){
+                int error {};
+                string sql;
+                sql =              string("SELECT FirstName, LastName, Age, Wage, StartDate, NULL AS StudyYear "
+                                   "FROM Employees "
+                                   "WHERE "
+                                   "UNION ALL "
+                                   "SELECT FirstName, LastName, Age, NULL AS Wage, StartDate, StudyYear "
+                                   "FROM Students "
+                                   ";");
+                Print_Query(User_id,sql,error);
+                break;
+    }
+
+    while(User_Capacity == "Student"){
+                int error {};
+                string sql;
+                sql =              string("SELECT C.CourseTitle, C.StudyYear, C.Semester, C.WritingMarkPerc, CE.OralMark, CE.WritingMark, CE.FinalMark "
+                                   "FROM CourseEnrolled as CE, Students as S, Courses as C "
+                                   "WHERE "
+                                   "S.S_id = CE.S_id AND "
+                                   "S.User_id = ") + to_string(User_id) + string(" AND "
+                                   "C.C_id = CE.C_id "
+                                   ";");
+                Print_Query(User_id,sql,error);
+                break;
+    }
+
+    while(User_Capacity == "Teacher"){
+                int error {};
+                string sql;
+                sql =              string("SELECT C.CourseTitle, C.StudyYear, C.Semester, CT.Day, CT.StartTime, CT.EndTime, C.WritingMarkPerc "
+                                   "FROM CourseTimetable as CT, Employees as E, Courses as C "
+                                   "WHERE "
+                                   "E.E_id = C.E_id AND "
+                                   "E.User_id = ") + to_string(User_id) + string(" AND "
+                                   "C.C_id = CT.C_id "
+                                   ";");
+                Print_Query(User_id,sql,error);
+                break;
+    }
+    while(User_Capacity == "Cleaner"){
+                int error {};
+                string sql;
+                sql =              string("SELECT CS.The_Date, CS.Shift "
+                                   "FROM CleaningSchedule as CS, Employees as E "
+                                   "WHERE "
+                                   "E.E_id = CS.E_id AND "
+                                   "E.User_id = ") + to_string(User_id) + string(" "
+                                   ";");
+                Print_Query(User_id,sql,error);
+                break;
     }
 
     return 0;
@@ -27,7 +131,7 @@ void init_db(){
     int exit = 0;
     exit = sqlite3_open("University_Platform.db", &DB);
 
-    if (exit) {
+    if (exit != SQLITE_OK) {
         cerr << "Error openning DB " << sqlite3_errmsg(DB) << endl;
         return;
     }
@@ -37,7 +141,7 @@ void init_db(){
                     "User_id INT ,"
                     "Username	TEXT NOT NULL,"
                     "Password	TEXT NOT NULL,"
-                    "Credentials	TEXT NOT NULL,"
+                    "Capacity	TEXT NOT NULL,"
                     "PRIMARY KEY(User_id)"
                     " );";
 
@@ -82,7 +186,7 @@ void init_db(){
                     "Age	INT,"
                     "Wage	INT,"
                     "StartDate  TEXT,"
-                    "Role TEXT ,"
+                    "Role  TEXT,"
                     "FOREIGN KEY(User_id) REFERENCES Users(User_id) ON DELETE CASCADE,"
                     "PRIMARY KEY(E_id)"
                     " );";
@@ -155,8 +259,8 @@ void init_db(){
         errors++;
     }
 
-    // Creating CleanningSchedule table
-    sql =           "CREATE TABLE IF NOT EXISTS CleanningSchedule ("
+    // Creating CleaningSchedule table
+    sql =           "CREATE TABLE IF NOT EXISTS CleaningSchedule ("
                     "The_Date TEXT ,"
                     "Shift TEXT ,"
                     "E_id	INT,"
@@ -167,7 +271,7 @@ void init_db(){
     exit = 0;
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
     if (exit != SQLITE_OK) {
-        cerr << "Error Creating CleanningSchedule Table" << endl;
+        cerr << "Error Creating CleaningSchedule Table" << endl;
         cout<< messaggeError << endl;
         sqlite3_free(messaggeError);
         errors++;
@@ -199,35 +303,35 @@ void bootstrap_db(){
     }
 
     // Inserting the User login info
-    sql   =     "INSERT INTO Users ( User_id, Username, Password, Credentials)"
+    sql   =     "INSERT INTO Users ( User_id, Username, Password, Capacity)"
                 "VALUES"
                 "(1,'admin','admin','admin'),"
                 "(2,'master_admin','master_admin','admin'),"
-                "(3,'GFanis','1234','User'),"
-                "(4,'Maria99','1234','User'),"
-                "(5,'XTrider','1234','User'),"
-                "(6,'chris_skater','1234','User'),"
-                "(7,'Kostas33','1234','User'),"
-                "(8,'Txrisa','1234','User'),"
-                "(9,'G_Papoulias','1234','User'),"
-                "(10,'Tazes','1234','User'),"
-                "(11,'Souka','1234','User'),"
-                "(12,'Nikos_Sherlock','1234','User'),"
-                "(13,'Nikos_Zen','1234','User'),"
-                "(14,'happyapple','1234','User'),"
-                "(15,'Katerina95','1234','User'),"
-                "(16,'Mitsaras','1234','User'),"
-                "(17,'Anna97','1234','User'),"
-                "(18,'Eleni_Star','1234','User'),"
-                "(19,'Panos_G4','1234','User'),"
-                "(20,'Giannis_G3','1234','User'),"
-                "(21,'Ntina99','1234','User'),"
-                "(22,'Filippos','1234','User'),"
-                "(23,'B_Pavlidou','1234','User'),"
-                "(24,'I_Papadakis','1234','User'),"
-                "(25,'G_Tsironis','1234','User'),"
-                "(26,'Ks_Zotos','1234','User'),"
-                "(27,'S_Trahanas','1234','User')"
+                "(3,'GFanis','1234','Student'),"
+                "(4,'Maria99','1234','Student'),"
+                "(5,'XTrider','1234','Student'),"
+                "(6,'chris_skater','1234','Student'),"
+                "(7,'Kostas33','1234','Student'),"
+                "(8,'Txrisa','1234','Student'),"
+                "(9,'G_Papoulias','1234','Student'),"
+                "(10,'Tazes','1234','Student'),"
+                "(11,'Souka','1234','Student'),"
+                "(12,'Nikos_Sherlock','1234','Student'),"
+                "(13,'Nikos_Zen','1234','Student'),"
+                "(14,'happyapple','1234','Student'),"
+                "(15,'Katerina95','1234','Student'),"
+                "(16,'Mitsaras','1234','Student'),"
+                "(17,'Anna97','1234','Student'),"
+                "(18,'Eleni_Star','1234','Employee'),"
+                "(19,'Panos_G4','1234','Employee'),"
+                "(20,'Giannis_G3','1234','Employee'),"
+                "(21,'Ntina99','1234','Employee'),"
+                "(22,'Filippos','1234','Employee'),"
+                "(23,'B_Pavlidou','1234','Employee'),"
+                "(24,'I_Papadakis','1234','Employee'),"
+                "(25,'G_Tsironis','1234','Employee'),"
+                "(26,'Ks_Zotos','1234','Employee'),"
+                "(27,'S_Trahanas','1234','Employee')"
                 "; ";
 
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
@@ -239,7 +343,7 @@ void bootstrap_db(){
     }
 
     // Inserting Student info
-    sql   =     "INSERT INTO Students (S_id, User_id, FirstName , LastName, Age, StartDate,StudyYear)"
+    sql   =     "INSERT INTO Students (S_id, User_id, FirstName , LastName, Age, StartDate, StudyYear)"
                 "VALUES"
                 "(1,3,'Fanis','Gioumatzidis',25,'09/2021',NULL),"
                 "(2,4,'Maria','Mpoutkari',24,'09/2021',NULL),"
@@ -260,7 +364,30 @@ void bootstrap_db(){
 
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
     if (exit != SQLITE_OK) {
-        cerr << "Error Insert Students" << endl;
+        cerr << "Error Inser\t Students" << endl;
+        cout << messaggeError << endl;
+        sqlite3_free(messaggeError);
+        errors++;
+    }
+
+    // Inserting Employee info
+    sql   =     "INSERT INTO Employees ( E_id, User_id, FirstName, LastName, Age, Wage, StartDate, Role)"
+                "VALUES"
+                "(1,23,'Basiliki','Pavlidou',50,3000,'09/1995','Teacher'),"
+                "(2,24,'Iosif','Papadakis',47,2500,'09/2010','Teacher'),"
+                "(3,23,'Giorgos','Tsironis',53,3300,'09/2000','Teacher'),"
+                "(4,26,'Ksenofon','Zotos',68,4000,'09/1985','Teacher'),"
+                "(5,27,'Stefanos','Trahanas',72,4200,'09/1980','Teacher'),"
+                "(6,18,'Eleni','Staridou',45,2700,'09/1999','Boarding'),"
+                "(7,19,'Panagiotis','Koutras',48,2500,'09/1990','Boarding'),"
+                "(8,20,'Ioannis','Trevlias',27,1500,'09/2018','Cleaner'),"
+                "(9,21,'Kostantina','Antoneli',29,1600,'09/2016','Cleaner'),"
+                "(10,22,'Filippos','Ioannou',35,1700,'09/2005','Cleaner')"
+                "; ";
+
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
+    if (exit != SQLITE_OK) {
+        cerr << "Error Insert Employees" << endl;
         cout << messaggeError << endl;
         sqlite3_free(messaggeError);
         errors++;
@@ -270,10 +397,10 @@ void bootstrap_db(){
     // Inserting Courses info
     sql   =     "INSERT INTO Courses ( C_id, E_id, CourseTitle , StudyYear,Semester,WritingMarkPerc)"
                 "VALUES"
-                "(1,23,'Electrodynamics',1,'Fall',7),"
-                "(2,25,'Introduction to Machine Learning',1,'Spring',8),"
-                "(3,26,'Computational Physics',2,'Fall',8),"
-                "(4,27,'Quantum Mechanics',2,'Spring',7)"
+                "(1,1,'Electrodynamics',1,'Fall',7),"
+                "(2,3,'Machine Learning',1,'Spring',8),"
+                "(3,4,'Classical Physics',2,'Fall',8),"
+                "(4,5,'Quantum Mechanics',2,'Spring',7)"
                 "; ";
 
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
@@ -380,7 +507,7 @@ void bootstrap_db(){
     }
 
     // Inserting The cleanning Schedule
-    sql   =     "INSERT INTO CleanningSchedule ( The_Date,Shift, E_id)"
+    sql   =     "INSERT INTO CleaningSchedule ( The_Date,Shift, E_id)"
                 "VALUES"
                 "('05/06/2024','Morning',8),"
                 "('05/06/2024','Afternoon',9),"
@@ -402,7 +529,7 @@ void bootstrap_db(){
 
     exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
     if (exit != SQLITE_OK) {
-        cerr << "Error Insert CleanningSchedule" << endl;
+        cerr << "Error Insert CleaningSchedule" << endl;
         cout << messaggeError << endl;
         sqlite3_free(messaggeError);
         errors++;
@@ -437,3 +564,148 @@ void bootstrap_db(){
     sqlite3_close(DB);
 
 }
+
+static int callback(void* data, int argc, char** argv, char** azColName) {
+    TableData* tableData = static_cast<TableData*>(data);
+
+    // If this is the first row, save the column names
+    if (tableData->columnNames.empty()) {
+        for (int i = 0; i < argc; i++) {
+            tableData->columnNames.push_back(azColName[i]);
+        }
+    }
+
+    // Save the row data
+    vector<string> row;
+    for (int i = 0; i < argc; i++) {
+        row.push_back(argv[i] ? argv[i] : "NULL");
+    }
+    tableData->rows.push_back(row);
+
+    return 0;
+}
+
+void login(int &User_id, string &User_Capacity, int &error){
+    // getting login info from user
+    string username{"_"}, password{"_"};
+    cout << "Please type your login info" << endl;
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+
+    if( username == "_" || password == "_" ){
+        error = 2;
+        return;
+    }
+
+
+    // Opening Database
+    sqlite3* DB;
+    sqlite3_stmt* stmt;
+    int exit = 0;
+    exit = sqlite3_open("University_Platform.db", &DB);
+    if (exit != SQLITE_OK) {
+        cerr << "Error opening DB " << sqlite3_errmsg(DB) << endl;
+        error = 3;
+        return;
+    }
+
+    // Preparing the SQL statement
+    string sql = "SELECT User_id,Capacity FROM Users WHERE username = ? AND password = ?;";
+    exit = sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, 0);
+    if ( exit != SQLITE_OK) {
+        cerr << "Failed to prepare statement: " << sqlite3_errmsg(DB) << endl;
+        sqlite3_close(DB);
+        error = 3;
+        return;
+    }
+
+    // Bind the username and password to the statement
+    sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
+
+    // Execute the statement and check if the username and password combination exists
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        User_id = sqlite3_column_int(stmt, 0); // Retrieve the User_id
+        User_Capacity = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)));// Retrieve the User_Capacity
+    }else{
+        error = 1;
+        sqlite3_finalize(stmt);
+        sqlite3_close(DB);
+        return;
+    }
+
+    // Finalize the first statement
+    sqlite3_finalize(stmt);
+    if (User_Capacity == "Employee"){
+        // Prepare the SQL statement to retrieve the Role
+        sql = "SELECT Role FROM Employees WHERE User_id = ?;";
+        exit = sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, 0);
+        if (exit != SQLITE_OK) {
+            cerr << "Failed to prepare statement: " << sqlite3_errmsg(DB) << endl;
+            sqlite3_close(DB);
+            error = 3;
+            return;
+        }
+
+
+        // Bind the User_id to the statement
+        sqlite3_bind_int(stmt, 1, User_id);
+
+        // Execute the statement and retrieve the Role
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            User_Capacity = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))); // Retrieve the Role
+        } else {
+            error = 4; // Error retrieving role
+            sqlite3_finalize(stmt);
+            sqlite3_close(DB);
+            return;
+        }
+        // Finalize the statement
+        sqlite3_finalize(stmt);
+    }
+
+    // Closing the database
+    sqlite3_close(DB);
+    error = 0;
+
+}
+
+void Print_Query(int User_id,string sql, int error){
+    sqlite3* DB;
+    int exit = 0;
+    exit = sqlite3_open("University_Platform.db", &DB);
+    if (exit) {
+        cerr << "Error opening DB: " << sqlite3_errmsg(DB) << endl;
+        error = 2;
+    }
+
+
+    TableData tableData;
+
+    int rc = sqlite3_exec(DB, sql.c_str(), callback, &tableData, NULL);
+    if (rc != SQLITE_OK) {
+        cerr << "Error SELECT: " << sqlite3_errmsg(DB)  << endl;
+        error = 3;
+    }
+
+        // Printing the table
+        int colWidth = 20;
+        for (const string& colName : tableData.columnNames) {
+            cout << setw(colWidth) << colName;
+        }
+        cout << endl;
+        cout << string(colWidth * tableData.columnNames.size(), '-') << endl;
+
+        for (const vector<string>& row : tableData.rows) {
+            for (const string& value : row) {
+                cout << setw(colWidth) << value;
+            }
+            cout << endl;
+        }
+
+
+    sqlite3_close(DB);
+}
+
